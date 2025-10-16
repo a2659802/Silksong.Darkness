0bde971bc5323a2766a0d1484bbd136992d28add
a2659802 <a2655536@163.com>
2025-10-16 23:22:06 +0800

    on enter hero hook impl

--- Silksong_DarknessPlugin.cs
+++ Silksong_DarknessPlugin.cs
@@ -1,14 +1,63 @@
 using BepInEx;
+using HarmonyLib;
+using HutongGames.PlayMaker;
+using UnityEngine;
 
 namespace Silksong.Darkness;
 
-// TODO - adjust the plugin guid as needed
 [BepInAutoPlugin(id: "io.github.silksong_darkness")]
 public partial class Silksong_DarknessPlugin : BaseUnityPlugin
 {
     private void Awake()
     {
-        // Put your initialization logic here
-        Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+        Logger.LogInfo($"[Darkness] Plugin {Name} ({Id}) has loaded!");
+        new Harmony(Id).PatchAll();
     }
 }
+
+[HarmonyPatch(typeof(GameManager), nameof(GameManager.EnterHero))]
+public static class Darkness_EnterHeroPatch
+{
+    public static void Postfix()
+    {
+        var hc = HeroController.instance;
+        if (hc == null || hc.vignetteFSM == null)
+            return;
+
+        hc.vignetteFSM.SetState("Dark 2");
+        hc.vignette.enabled = true;
+
+        Debug.Log("[Darkness_EnterHeroPatch] Forced vignette to Dark 2.");
+
+        FsmState? dark2 = null;
+
+        foreach (var state in hc.vignetteFSM.FsmStates)
+        {
+            if (state.Name == "Dark 2")
+            {
+                dark2 = state;
+                break;
+            }
+        }
+        if (dark2 == null)
+        {
+            Debug.LogError("[Darkness_EnterHeroPatch] Could not find 'Dark 2' state in vignette FSM.");
+            return;
+        }
+
+        foreach (var state in hc.vignetteFSM.FsmStates)
+        {
+            if (state.Name == "Dark Lev Check")
+            {
+                foreach (var trans in state.Transitions)
+                {
+                    trans.ToState = "Dark 2";
+                    trans.ToFsmState = dark2;
+                }
+            }
+        }
+        
+         Debug.Log("[Darkness_EnterHeroPatch] change transition rule");
+    }
+}
+
