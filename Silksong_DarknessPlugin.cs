898285a14d3b4015388ee797cd77537fb644dd11
a2659802 <a2655536@163.com>
2025-10-18 00:09:55 +0800

    fix: 修复某些场景不生效的问题

--- Silksong_DarknessPlugin.cs
+++ Silksong_DarknessPlugin.cs
@@ -2,6 +2,7 @@ using BepInEx;
 using HarmonyLib;
 using HutongGames.PlayMaker;
 using UnityEngine;
+using HutongGames.PlayMaker.Actions;
 
 namespace Silksong.Darkness;
 
@@ -43,6 +44,9 @@ public static class VignetteFSM_AwakePatch
             Debug.LogError("[Darkness] Could not find 'Dark 2' and 'Dark 2 2' states.");
             return;
         }
+        // 缩小光圈范围，使其更暗一些
+        changeDarkAction(dark2);
+        changeDarkAction(dark22);
 
         // 修改需要固定跳转的状态
         foreach (var state in __instance.FsmStates)
@@ -65,6 +69,58 @@ public static class VignetteFSM_AwakePatch
                 }
                 Debug.Log("[Darkness] Transition fixed and vignette forced Dark 2 2.");
             }
-        }        
+        }
+    }
+    private static void changeDarkAction(FsmState dark)
+    {
+
+        // Idle Scale
+        var idleScaleAction = dark.GetAction<SetVector3XYZ>(1);
+        if (idleScaleAction == null)
+        {
+            Debug.LogError("[Darkness] Could not find SetVector3XYZ action in Dark state.");
+            return;
+        }
+        idleScaleAction.x = 1.2f; // 原本的值是1.8f
+        idleScaleAction.y = 1.6f; // 原本的值是1.8f
+        idleScaleAction.z = 1.6f; // 原本的值是1.8f
+        // Damage Scale
+        var damageScaleAction = dark.GetAction<SetVector3XYZ>(2);
+        if (damageScaleAction == null)
+        {
+            Debug.LogError("[Darkness] Could not find SetVector3XYZ action in Dark state.");
+            return;
+        }
+        damageScaleAction.x = 1.1f; // 原本的值是1.6f
+        damageScaleAction.y = 1.5f; // 原本的值是1.6f
+        damageScaleAction.z = 1.5f; // 原本的值是1.6f
+    }
+    private static T?GetAction<T>(this FsmState state, int actionIndex) where T : FsmStateAction
+    {
+        if (actionIndex < 0 || actionIndex >= state.Actions.Length)
+        {
+            return null!;
+        }
+
+        return state.Actions[actionIndex] as T;
+    }
+}
+
+
+[HarmonyPatch(typeof(GameManager), nameof(GameManager.EnterHero))]
+public static class Darkness_EnterHeroPatch
+{
+    public static void Postfix()
+    {
+        // 查找所有名字包含Vignette Cutout的游戏对象并禁用它们
+        var vignetteObjects = Object.FindObjectsByType<GameObject>(FindObjectsSortMode.None);
+        foreach (var obj in vignetteObjects)
+        {
+            if (obj.name.Contains("Vignette Cutout"))
+            {
+                obj.SetActive(false);
+                Debug.Log("[Darkness] Disabled Vignette Cutout object: " + obj.name);
+            }
+        }
     }
 }
